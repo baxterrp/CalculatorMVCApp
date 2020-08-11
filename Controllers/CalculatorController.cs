b72fa59c2eb14a41c18b0e901db4aff337986285
SRCummings1 <45797938+SRCummings1@users.noreply.github.com>
2020-08-11 18:13:01 -0400

    creating repo to test calculator code

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPCALC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCALC.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Calc());
+        }
+        [HttpPost]
+        public ActionResult Index(Calc c, string calculate)
+        {
+            if (calculate == "add")
+            {
+                c.total = c.number1 + c.number2;
+            }
+            else if (calculate == "min")
+            {
+                c.total = c.number1 - c.number2;
+            }
+            else if (calculate == "sub")
+            {
+                c.total = c.number1 * c.number2;
+            }
+            else
+            {
+                c.total = c.number1 - c.number2;
+            }
+            return View(c);
+        }
+    }
+}
---
+++ Models/Calc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCALC.Models
+{
+    public class Calc
+    {
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+        public int total { get; set; }
+    }
+}
